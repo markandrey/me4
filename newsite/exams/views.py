a52259b58c898b08de7b91ba768251b3496ac5aa
Andrey Mark <markandreym@gmail.com>
2024-02-10 07:22:30 +0300

    mainmenu

--- newsite/exams/views.py
+++ newsite/exams/views.py
@@ -1,6 +1,6 @@
 from django.contrib import auth
 # from django.http import HttpResponse
-# from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 # from django.urls import reverse_lazy
 from django.views.generic import FormView, CreateView, TemplateView, ListView
 #
@@ -12,5 +12,37 @@ from .utils import menu, DataMixin
 class First(DataMixin, TemplateView):
     template_name = 'exams/index.html'
     title_page = 'Главная страница'
-    # message = f"Добро пожаловать на сайт! Уже зарегистрировано участников!"
     message = f"Добро пожаловать на сайт! Уже зарегистрировано {auth.get_user_model().objects.count()} участников!"
+
+
+def show_all(request):
+    # all_exams = Exam.objects.filter(patient=request.user.pk)
+    all_exams = []
+    # all_ans = Analysis.objects.filter(patient=request.user.pk)
+    all_ans = []
+
+    data = {
+        'title': 'Вся медицинская информация.',
+        'menu': menu,
+        "all_exams": all_exams,
+        "all_ans": all_ans,
+    }
+    return render(request,"exams/show_all.html", context=data)
+
+
+def info(request):
+    # if request.method == 'POST':
+    #     form = UploadFileForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         fp = UploadFiles(file=form.cleaned_data['file'], patient=request.user)
+    #         fp.save()
+    # else:
+    #     form = UploadFileForm()
+    form = ''
+
+    data = {
+        'title': 'Добавление результата',
+        'menu': menu,
+        'form': form,
+    }
+    return render(request, 'exams/file_upload.html', context=data)
